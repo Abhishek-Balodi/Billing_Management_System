907936543b54e6a1bfc31abc076aca9697503cc8
Abhishek Balodi <abhishekbalodi1189@gmail.com>
2025-10-13 13:34:21 +0530

    purchase_detail model

---
+++ app/Models/PurchaseDetail.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PurchaseDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'invoice_no',
+        'invoice_date',
+        'supplier_id',
+        'user_id',
+        'employee_id',
+        'status',
+        'purchase_type',
+        'challan_no',
+        'challan_date',
+        'lr_no',
+        'entry_date',
+        'delivery_mode',
+        'total_amount',
+        'discount_amount',
+        'tax_amount',
+        'grand_total',
+        'remarks',
+        'created_by',
+    ];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(PurchaseItem::class, 'purchase_id');
+    }
+}
