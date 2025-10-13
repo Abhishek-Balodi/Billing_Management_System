da5c238a617c4f01be94864598681b49e6695168
Abhishek Balodi <abhishekbalodi1189@gmail.com>
2025-10-13 13:37:38 +0530

    purchase_item model

---
+++ app/Models/PurchaseItem.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PurchaseItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'purchase_id',
+        'product_id',
+        'product_name',
+        'hsn_code',
+        'qty',
+        'unit',
+        'price',
+        'user_id',
+        'employee_id',
+        'discount',
+        'tax_percent',
+        'igst_percent',
+        'tax_amount',
+        'total_amount',
+        'expiry_date',
+    ];
+
+    public function purchase()
+    {
+        return $this->belongsTo(PurchaseDetail::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
