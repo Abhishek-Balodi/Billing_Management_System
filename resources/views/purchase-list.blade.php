d62bca60a499fffa529e0c662b72e43eb2e1f9b2
Abhishek Balodi <abhishekbalodi1189@gmail.com>
2025-12-01 12:23:31 +0530

    delete modal added

--- resources/views/purchase-list.blade.php
+++ resources/views/purchase-list.blade.php
@@ -259,6 +259,27 @@
 </div>
 
 @include('layouts.footer')
+
+	<!-- delete modal -->
+	<div class="modal fade" id="delete-modal">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="page-wrapper-new p-0">
+					<div class="content p-5 px-3 text-center">
+							<span class="rounded-circle d-inline-flex p-2 bg-danger-transparent mb-2"><i class="ti ti-trash fs-24 text-danger"></i></span>
+							<h4 class="fs-20 fw-bold mb-2 mt-1">Delete Purchase</h4>
+							<p class="mb-0 fs-16">Are you sure you want to delete purchase?</p>
+							<div class="modal-footer-btn mt-3 d-flex justify-content-center">
+								<button type="button" class="btn me-2 btn-secondary fs-13 fw-medium p-2 px-3 shadow-none" data-bs-dismiss="modal">Cancel</button>
+								<button type="submit" class="btn btn-submit fs-13 fw-medium p-2 px-3">Yes Delete</button>
+							</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script>
 $(document).ready(function() {
